eec7c9d169e5947f03ae3b0c44a7c51f79773281
Max.Chou <v2asault@yahoo.com.tw>
2023-09-20 09:46:00 +0800

    update url

--- app.js
+++ app.js
@@ -1,7 +1,7 @@
 var init = (function () {
   const referer = document.referrer;
 
-  if (referer.includes("salesforce-experience.com")) {
+  if (referer.includes("salesforce-experience.com") || referer.includes('sandbox.my.site.com')) {
     const iframeElement = document.createElement("iframe");
     iframeElement.src =
       "https://docs.google.com/forms/d/e/1FAIpQLScQpVWXXXVkXR3jj8INR9EKDquUUYi8lLisjDMVlDxhtmS3Ow/viewform?embedded=true";
@@ -9,5 +9,7 @@ var init = (function () {
     iframeElement.height = 600;
 
     document.querySelector("#app").appendChild(iframeElement);
+  } else {
+    document.querySelector("#app").innerHTML = 'Access denied'
   }
 })();
