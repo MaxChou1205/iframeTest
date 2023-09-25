ae3311303d82216c33be67d702921183d7d11201
MaxChou1205 <v2asault@yahoo.com.tw>
2023-09-25 23:20:23 +0800

    wordpress test

--- app.js
+++ app.js
@@ -1,15 +1,19 @@
 var init = (function () {
   const referer = document.referrer;
 
-  if (referer.includes("salesforce-experience.com") || referer.includes('sandbox.my.site.com')) {
+  if (
+    referer.includes("salesforce-experience.com") ||
+    referer.includes("sandbox.my.site.com")
+  ) {
     const iframeElement = document.createElement("iframe");
-    iframeElement.src =
-      "https://docs.google.com/forms/d/e/1FAIpQLScQpVWXXXVkXR3jj8INR9EKDquUUYi8lLisjDMVlDxhtmS3Ow/viewform?embedded=true";
+    // iframeElement.src =
+    // "https://docs.google.com/forms/d/e/1FAIpQLScQpVWXXXVkXR3jj8INR9EKDquUUYi8lLisjDMVlDxhtmS3Ow/viewform?embedded=true";
+    iframeElement.src = "https://v2asault.wordpress.com/";
     iframeElement.width = 800;
     iframeElement.height = 600;
 
     document.querySelector("#app").appendChild(iframeElement);
   } else {
-    document.querySelector("#app").innerHTML = 'Access denied'
+    document.querySelector("#app").innerHTML = "Access denied";
   }
 })();
